c6119d71d3101f317951f31875e6ecf353d7c520
niyarin <niyarium@gmail.com>
2018-08-31 00:44:06 +0900

    first commit

---
+++ examples/sample.scm
@@ -0,0 +1,31 @@
+(include "../niyarin_optparse/niyarin_optparse.scm")
+
+(import (scheme base)
+        (scheme write)
+        (niyarin optparse))
+
+(define (print-help option)
+  (display  (niyarin-optparse-generate-help-text option '(program-name "test program"))))
+
+(define (run option)
+  (display "do something")
+  (newline)
+  (write option)
+  (newline)
+  )
+
+(define (main)
+   (let ((cmd-option
+           '(("--help" "-h" (help "Display a help message and exit."))
+             ("--foo" (destination "INPUT") (nargs 1) (help "foooooooooooooo"))
+             ("--foo2" (destination "INPUT") (nargs *))
+             ("--foo3 " (nargs 3))
+             ("files" (nargs *))
+             )))
+     (let ((parsed-option (niyarin-optparse-optparse cmd-option)))
+       (if (assoc "--help" parsed-option string=?)
+         (print-help cmd-option)
+         (run parsed-option)
+         ))))
+
+(main)
